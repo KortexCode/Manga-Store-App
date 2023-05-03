cdf9bf8c28a7a6cfb9aba309ca8f3e8a344b0f3a
kortexcode <darkmaeno323@gmail.com>
2023-05-03 16:29:58 -0500

    Se instalan dependencias para husky y lint-staged, se crean archivos de configuración

---
+++ src/containers/App.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react';
+
+function App() {
+
+    useEffect(()=>{
+        (async function(){
+            const response = await fetch('https://api.jikan.moe/v4/manga');
+            const data = await response.json();
+            console.log("res", data)
+        })()
+    })
+    return (
+      <h1>Hola chikorita</h1>
+    )
+}
+
+export {App}
