c947f0bd18950f815744152388698aee91299c2d
kortexcode <darkmaeno323@gmail.com>
2023-05-05 23:02:29 -0500

    Se agregan estilos al home en la sección de manga y estilos a las Cards de mangas

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { App } from './routes/App';
 import './styles/main.css';
 
-const x = false;
-console.log('hola', x);
-
 const nodeRoot: HTMLElement | null = document.getElementById('root');
 
 const root = ReactDOM.createRoot(nodeRoot as Element);
