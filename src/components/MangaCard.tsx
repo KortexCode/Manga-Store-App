95e163b361b557646ba56530b7b5551e39221b2a
kortexcode <darkmaeno323@gmail.com>
2023-05-05 23:02:56 -0500

    Estilos adicionales a las cards y sección de manga

--- src/components/MangaCard.tsx
+++ src/components/MangaCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Datum } from '../constants/types/mangas';
+import '../styles/MangaCard.scss';
 
 type Props = {
 	item: Datum | null;
@@ -9,8 +10,12 @@ function MangaCard(props: Props) {
 	const { item } = props;
 	return (
 		<article className='MangaCard'>
-			<img className='MangaCard__img' src='' alt='Manga' />
-			<div className='MangaCard__description'>
+			<img
+				className='MangaCard__img'
+				src={item?.images.jpg.image_url}
+				alt={item?.title}
+			/>
+			<div className='MangaCard__description-container'>
 				<p className='MangaCard__title'>{item?.title}</p>
 				<p className='MangaCard__rank'>Rank</p>
 			</div>
