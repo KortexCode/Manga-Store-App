f9a5081e628792289d48782262531544419b6cfa
kortexcode <darkmaeno323@gmail.com>
2023-05-07 08:50:08 -0500

    Cambios a los estilos del header y el home

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
+import { HiShoppingCart } from 'react-icons/hi';
 import '../styles/Header.scss';
 
 function Header(): JSX.Element {
 	return (
 		<header className='Header'>
-			<nav className='Header__nav'>
-				<span className='Header__title'>Manga Hakken</span>
-				<ul>
-					<li>Home</li>
-					<li>Contact</li>
-					<li>About us</li>
-				</ul>
-				<div>
-					<p>Checkout</p>
+			<nav className='nav-bar'>
+				<span className='nav-bar__title'>Manga Hakken</span>
+				<div className='nav-bar__items-conatiner'>
+					<ul className='nav-bar__items'>
+						<li>Home</li>
+						<li>Contact</li>
+						<li>About us</li>
+					</ul>
+					<span className='nav-bar__shopping-cart'>
+						<HiShoppingCart size={28} />
+						<div className='shopping-cart__count' />
+					</span>
 				</div>
 			</nav>
 		</header>
